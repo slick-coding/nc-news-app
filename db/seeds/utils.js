d92face32fdfbd51670d851b14fecf9afa65ebcb
slick-coding <alick.doggett@yahoo.co.uk>
2025-03-10 19:10:35 +0000

    add checkExists function for verifying SQL inputs

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -1,4 +1,5 @@
 const db = require("../../db/connection");
+const format = require("pg-format")
 
 exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
     if (!created_at) return { ...otherProperties };
@@ -11,4 +12,12 @@ exports.createLookupObject = (database, targetKey, targetValue) => {
         lookup[database[i][targetKey]] = database[i][targetValue];
     }
     return lookup;
+};
+
+exports.checkExists = (table, column, value) => {
+    const queryString = format("SELECT * FROM %I WHERE %I = $1", table, column);
+    return db.query(queryString, [value]).then(({ rows }) => {
+        if (rows.length === 0)
+            return Promise.reject({ status: 404, msg: "Not found" });
+    });
 };
