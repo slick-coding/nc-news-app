9fbf07ded223a66f707d823551c6e0b458d8d996
slick-coding <alick.doggett@yahoo.co.uk>
2025-03-10 16:37:45 +0000

    add 404 & 500 error handling plus test

--- app.js
+++ app.js
@@ -5,4 +5,15 @@ const app = express();
 
 app.get("/api", getApi)
 
+
+// Error Endpoints
+app.all("*", (req, res, next) => {
+    res.status(404).send({ msg: "Path not found" });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send("Something went wrong");
+});
+
 module.exports = app;
