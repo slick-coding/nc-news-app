416e2d39eb7aaa96f71c67c1cb3ca9733308c911
slick-coding <alick.doggett@yahoo.co.uk>
2025-03-12 10:46:12 +0000

    add error handling for /api/articles/author_id/comments

--- errors/errors.js
+++ errors/errors.js
@@ -4,6 +4,10 @@ exports.handleCustomErrors = (err, req, res, next) => {
 };
 
 exports.handlePsqlErrors = (err, req, res, next) => {
-    if (err.code === "22P02") res.status(400).send({ msg: "Bad request" });
+    if (err.code === "22P02" || err.code === "23502") {
+        res.status(400).send({ msg: "Bad request" });
+    } else if (err.code === "23503") {
+        res.status(404).send({ msg: "Not found" });
+    }
     next(err);
 };
