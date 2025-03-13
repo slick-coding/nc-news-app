0af26431666ec568e97db1648ffa3c113ec8da70
slick-coding <alick.doggett@yahoo.co.uk>
2025-03-13 14:16:33 +0000

    add error handling and tests

--- __tests__/comments.test.js
+++ __tests__/comments.test.js
@@ -14,9 +14,22 @@ describe("DELETE /api/comments/:comment_id", () => {
             .delete("/api/comments/1")
             .expect(204)
             .then(() => {
-                return db.query("SELECT * FROM comments").then(({rows}) => {
+                return db.query("SELECT * FROM comments").then(({ rows }) => {
                     expect(rows.length).toBe(17);
                 });
             });
+    });
+});
+
+describe("Error Handling", () => {
+    describe("DELETE /api/comments/comment_id", () => {
+        test("404: Responds with error when given an invalid article_id", () => {
+            return request(app)
+                .delete("/api/comments/123")
+                .expect(404)
+                .then(({ body }) => {
+                    expect(body.msg).toBe("Not found");
+                });
+        });
     });
 });
--- models/comments.models.js
+++ models/comments.models.js
@@ -3,11 +3,6 @@ const format = require("pg-format");
 const { checkExists } = require("../db/seeds/utils");
 
 exports.removeCommentById = (comment_id) => {
-    console.log(comment_id);
-
-    // db.query(`DELETE FROM comments WHERE comment_id = $1`, [comment_id]),
-    // checkExists("comments", "comment_id", comment_id),
-
     return checkExists("comments", "comment_id", comment_id).then(() => {
         return db.query(`DELETE FROM comments WHERE comment_id = $1`, [comment_id]).then(() => {
             console.log("Deletion successful");
