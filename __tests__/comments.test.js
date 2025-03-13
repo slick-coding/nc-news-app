6663c17ba55fb5ee06ef02ebc9d3a59bf96f9ebf
slick-coding <alick.doggett@yahoo.co.uk>
2025-03-13 13:32:56 +0000

    add testing for DELETE /api/comments/:comment_id

--- __tests__/comments.test.js
+++ __tests__/comments.test.js
@@ -7,3 +7,16 @@ const data = require("../db/data/test-data/index");
 beforeAll(() => seed(data));
 
 afterAll(() => db.end());
+
+describe("DELETE /api/comments/:comment_id", () => {
+    test("204: deletes the specified comment", () => {
+        return request(app)
+            .delete("/api/comments/1")
+            .expect(204)
+            .then(() => {
+                return db.query("SELECT * FROM comments").then((result) => {
+                    expect(result.length).toBe(17);
+                });
+            });
+    });
+});
