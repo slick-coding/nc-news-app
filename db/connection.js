506bc62f43fab229a3605a487ed8a7c77c35d15f
slick-coding <alick.doggett@yahoo.co.uk>
2025-03-14 15:02:11 +0000

    add setup for SUPABASE

--- db/connection.js
+++ db/connection.js
@@ -6,40 +6,18 @@ require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 const db = new Pool();
 
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not configured");
 } else {
     console.log(`Connected to ${process.env.PGDATABASE}`);
 }
 
-/*
+const config = {};
 
-function findUsers() {
-    return db.query(`SELECT username FROM users`).then((response) => {
-        console.log(response.rows);
-    });
+if (ENV === "production") {
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2;
 }
 
-function findCodingArticles() {
-    return db.query(
-        `SELECT title FROM articles
-        WHERE topic='coding' `
-    ).then((response) => {
-        console.log(response.rows);
-    });
-}
-
-function findBadComments() {
-    return db.query(
-        `SELECT comment_id, votes, author FROM comments
-        WHERE votes < 0`
-    ).then((response) => {
-        console.log(response.rows);
-    });
-}
-
-findUsers()
-findCodingArticles()
-findBadComments()
-*/
 module.exports = db;
+module.exports = new Pool(config);
--- listen.js
+++ listen.js
@@ -1,10 +1,11 @@
 const app = require("./app");
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, (err) => {
+app.listen(PORT, (err) => {
     if (err) {
         console.log(err);
     } else {
-        console.log("Connected to port 9090");
+        console.log(`Connected to ${PORT}...`);
     }
 });
 
